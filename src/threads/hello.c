f740da19d6eb8f9106e3531760687b1b95ac8dd3
Anupam-pandey <pandey.anupam92@gmail.com>
2017-11-26 01:53:29 +0530

    Solved two problems

---
+++ src/threads/hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void test_hello(void)
+{
+	printf("\n*******************************************\n\n");
+	printf("Hello User!!!\nWelcome to Pintos\n\n");
+	printf("*******************************************\n\n");
+}
---
+++ src/threads/hello.h
@@ -0,0 +1,6 @@
+#ifndef SRC_THREADS_HELLO_H_
+#define SRC_THREADS_HELLO_H_
+
+void test_hello(void);
+
+#endif /* SRC_THREADS_HELLO_H_ */
